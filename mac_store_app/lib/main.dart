d72ea366e615728789f54fb3cf256571d86d7db8
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2025-03-01 07:54:33 +0300

    Firebase integration added.

--- mac_store_app/lib/main.dart
+++ mac_store_app/lib/main.dart
@@ -1,7 +1,21 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mac_store_app/views/screens/authentication_screens/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: 'AIzaSyDd-7ZleXI-wI5tkf-ws6_HQdpotvvog0A',
+              appId: '1:827618599469:android:0a088523f52de5a3becea4',
+              messagingSenderId: '827618599469',
+              projectId: 'store-app-95614',
+              storageBucket: 'gs://store-app-95614.firebasestorage.app'),
+        )
+      : Firebase.initializeApp();
   runApp(const MyApp());
 }
 
