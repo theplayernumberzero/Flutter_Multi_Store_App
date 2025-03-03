eb0be7d28b548abcac9277db71acba7b5fcd0c20
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2025-03-03 08:14:14 +0300

    Working on the header

--- mac_store_app/lib/views/screens/nav_screens/home_screen.dart
+++ mac_store_app/lib/views/screens/nav_screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mac_store_app/views/screens/nav_screens/widgets/header_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,8 +7,12 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home Screen"),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            HeaderWidget(),
+          ],
+        ),
       ),
     );
   }
---
+++ mac_store_app/lib/views/screens/nav_screens/widgets/header_widget.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class HeaderWidget extends StatelessWidget {
+  const HeaderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.2,
+      child: Stack(
+        children: [
+          Image.asset(
+            'assets/icons/searchBanner.jpeg',
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            left: 48,
+            top: 68,
+            child: SizedBox(
+              width: 250,
+              height: 50,
+              child: TextField(
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey.shade200,
+                    focusColor: Colors.black,
+                    prefixIcon: Image.asset('assets/icons/searc1.png'),
+                    suffixIcon: Image.asset('assets/icons/cam.png'),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+                    hintText: 'Enter text',
+                    hintStyle:
+                        TextStyle(fontSize: 14, color: Color(0xFF7F7F7F))),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 311,
+            top: 78,
+            child: Material(
+              type: MaterialType.transparency,
+              child: InkWell(
+                //overlayColor: MaterialStateProperty.all(Color(0x0c7f7f)),
+                onTap: () {},
+                child: Ink(
+                  //Tıklama efekti
+                  width: 31,
+                  height: 31,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/icons/bell.png'))),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+              left: 354,
+              top: 78,
+              child: Material(
+                type: MaterialType.transparency,
+                child: InkWell(
+                  onTap: () {},
+                  child: Ink(
+                    width: 31,
+                    height: 31,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/icons/message.png'),
+                      ),
+                    ),
+                  ),
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
